5d1570f67fea14e50b5555ecf378d582cc5da621
Rasalguul <66523118+Rasalguul@users.noreply.github.com>
2026-09-04 18:49:27 +0200

    Create cv-data.js
    feat: add CV data structure

---
+++ js/cv-data.js
@@ -0,0 +1,11 @@
+// Datos del CV.
+// Los datos reales se añadirán posteriormente.
+
+const cvData = {
+  personal: {},
+  experiencia: [],
+  formacion: [],
+  habilidades: [],
+  proyectos: [],
+  contacto: {}
+};
