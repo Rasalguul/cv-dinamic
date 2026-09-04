d95fbe86da307d15dc96d9600b0f2df24cd72c06
Rasalguul <66523118+Rasalguul@users.noreply.github.com>
2026-09-04 18:48:38 +0200

    Create navigation.js
    feat: add navigation javascript

---
+++ js/navigation.js
@@ -0,0 +1,8 @@
+// Lógica de navegación.
+// Se ampliará cuando definamos la navegación definitiva del CV.
+
+document.addEventListener("DOMContentLoaded", () => {
+  const currentPage = window.location.pathname;
+
+  console.log("Página actual:", currentPage);
+});
